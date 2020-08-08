79131b8282cdc1e31ebd5bc54184783661fbd926
charles <charles0260@outlook.com>
2020-08-08 14:14:39 +0800

    - change layout - add smooth scrollbar

--- src/app.js
+++ src/app.js
@@ -1,3 +1,31 @@
 import "./scss/style.scss";
-import "bootstrap"
+import "bootstrap";
+import Scrollbar from 'smooth-scrollbar';
+import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
+
+
+
+Scrollbar.use(OverscrollPlugin);
+
+
+var options = {
+    damping: 0.07,
+    plugins: {
+        overscroll: {
+            damping: 0.2,
+            effect: 'bounce',
+            maxOverscroll: 80,
+            // effect : 'glow',
+            // glowColor: '#000000'
+        }  
+      },
+
+};
+
+  Scrollbar.init(document.querySelector('.smoothScrollBar'), 
+  options
+  );
+
 console.log('hello') ;
+
+
